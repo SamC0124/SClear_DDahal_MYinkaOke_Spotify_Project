c783340245ebb49a69569c11088b940d46fee3e9
SamC0124 <sclear@uvm.edu>
2024-03-22 14:36:08 -0400

    Added section from Diwas

--- main.py
+++ main.py
@@ -14,6 +14,7 @@ import string
 import sklearn as sk
 from sklearn.linear_model import LinearRegression
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.cluster import KMeans
 
 # Main function
 # Questions we want to answer: What features are the most effective at predicting whether a song is popular on Spotify?
@@ -24,57 +25,66 @@ from sklearn.ensemble import RandomForestClassifier
 # However, we also have a prompt offered around predicting what songs people would be interested based on their danceability and valence scores:
 # - What do we need to know what songs would fit a person's danceability and valence scores? What does this have to do with popularity scores?
 # - A person isn't necessarily going to like a song based on what everyone else likes in a song, people's perception's
-#   of a songs dancability can be variable for one particular song. This can't really be shown with our project, since
+#   of a songs danceability can be variable for one particular song. This can't really be shown with our project, since
 #   we have the average popularity scores for each popular song, but I believe it would confound our project's ability
 #   to return songs that a person would actually like.
 
 
 if __name__ == '__main__':
     data = pd.read_csv('data/song_data.csv').dropna()
-    print(data.describe())
+    # print(data.describe())
     print("Some of the data is unnamed, has no popularity, tempo, or time signature. This data is likely null.")
 
-    # Between 1-3 different entries don't have an artist, album name, or track_name. Are these columns what we want to use to evaluate the relationship between different characteristics in songs, Popularity, and Dancability?
+    # Between 1-3 different entries don't have an artist, album name, or track_name. Are these columns what we want to
+    # use to evaluate the relationship between different characteristics in songs, Popularity, and Dancability?
     # Another thing to note is that some tracks are entered into the dataset more than once. These songs will be
     # overrepresented in the dataset, so we can just take unique values by track ID.
-    print(data.info())
+    # print(data.info())
 
-    # unique_data = data[data['track_id'].unique()]
-    # unique_data.info()
+    # Removing all columns with non-unique values
+    unique_data = data.drop_duplicates('track_id')
 
-    # Plotting Danceability by Popularity
-    # plt.hist(data=data)
-    # plt.show()
+    # TODO: Remove the column "Unnamed: 0" which acts as the index. We will have a unique key with the track_id section, and
+    print(unique_data.corr())
+    unique_data.drop(unique_data.columns[0])
+    print(unique_data.columns)
 
-    print(data.corr()['popularity'])
+    # What clusters are returned from clustering the data?
+    clusters = KMeans(n_clusters=5, max_iter=1000, random_state=1).fit(unique_data[['danceability', 'explicit', 'instrumentalness']])
+    print(clusters.labels_)
 
-    # Split data into training and testing sets (adjust test_size and random_state as needed)
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
+    unique_data["possible_groups"] = clusters.labels_
+    print(unique_data.corr())
 
-    # Tokenization using CountVectorizer (you can also use TF-IDF vectorizer)
-    X_train_vectorized, X_test_vectorized = create_bag_of_words(X_train, X_test)
+    if __name__ == '__main__':
+        # Load data and drop rows with empty values
 
-    # #Try SMOTE to fix class imbalance -- *** tried, made precision, recall, accuracy worse
-    # X_train_vectorized, y_train = use_smote(X_train_vectorized, y_train)
+        # print(data.describe())
 
-    # Train RF classifier
-    rf_classifier = RandomForestClassifier(n_estimators=100, random_state=0)
-    rf_classifier.fit(X_train_vectorized, y_train)
+        # Load data and drop unnecessary columns and rows with null values
+        data = pd.read_csv('data/song_data.csv').drop(columns=['track_id', 'key']).dropna()
 
-    # Predict the labels for test data
-    y_pred = rf_classifier.predict(X_test_vectorized)
+        # Remove duplicates based on track name
+        data = data.drop_duplicates(subset=['track_name'])
 
-    # Evaluate this classifier
-    accuracy = accuracy_score(y_test, y_pred)
-    report = classification_report(y_test, y_pred)
-    conf_matrix = confusion_matrix(y_test, y_pred)
+        # Filter data with "popularity" greater than 75
+        '''we consider a song is popular if it's popularity determined by the number of time it was played is greater than 75
+        * also if we want we can increase the number of popularity for better accuracy'''
 
-    print(f'Accuracy: {accuracy}')
-    print(f'Classification Report:\n{report}')
-    print("Confusion Matrix:")
-    print(conf_matrix)
+        data = data[data['popularity'] > 75]
 
-    # for row in range(len(data)):
-    #     if data['artists'][row].isnull():
-    #         pass
-    # pass
+        # Calculate average of specified columns
+        '''using that data we find the average the durations_ms, danceability, 
+        energy, loudness and any other factors that can help us make a model to predict the popularity of a song'''
+
+        average_data = data[['duration_ms', 'danceability', 'energy', 'loudness',
+                             'speechiness', 'acousticness', 'instrumentalness', 'liveness', 'valence', 'tempo',
+                             'time_signature']].mean()
+
+        '''using the average we set the upper limit/threshold to predict the popularity of a song
+        * once our model is created we can see what features make the song popular 
+        * we would most likely need create some sort of classification to check the features '''
+
+        print(average_data)
+
+
